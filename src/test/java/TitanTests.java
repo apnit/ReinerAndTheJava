42fa5216ee6627b835b8fda907a9584a91afc729
abbasam <tiggerwitht1378@gmail.com>
2022-04-14 11:42:07 +0430

    Simple Test

--- src/test/java/TitanTests.java
+++ src/test/java/TitanTests.java
@@ -1,5 +1,12 @@
 import org.junit.jupiter.api.Test;
+import titans.ArmoredTitan;
 
 public class TitanTests {
-    // TODOg
+    public static void main(String[] args) {
+        ArmoredTitan Reiner = ArmoredTitan.create();
+        System.out.println(Reiner.getFaceOrgan().getHp());
+        Reiner.getFaceOrgan().takeDamage(20);
+        System.out.println(Reiner.getFaceOrgan().getHp());
+
+    }
 }
